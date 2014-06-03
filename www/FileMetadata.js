77184af16beaababa1cae2110e64cc5a58a3d18f
o.zambotti <o.zambotti@trentorise.eu>
2014-06-03 16:23:12 +0200

    Under heavy development

--- www/FileMetadata.js
+++ www/FileMetadata.js
@@ -2,18 +2,22 @@
  * Constructor.
  * localURL {DOMString}
  */
-var FileMetadata = function(localURL){
-    this.localURL = localURL || null;
+var FileMetadata = function (localURL) {
+  this.localURL = localURL || null;
 };
 
 
 /**
  * Prints on console the provided message.
  */
-FileMetadata.prototype.test = function(msg) {
-	console.log('FileMetadata MSG: '+msg);
-    return;
+FileMetadata.prototype.test = function (msg) {
+  console.log('FileMetadata MSG: ' + msg);
+  return;
 };
 
+FileMetadata.prototype.metadata = function (filepath, successCallback, errorCallback) {
+  exec(successCallback, errorCallback, 'FileMetadata', 'metadata', [filepath]);
+  return;
+};
 
 module.exports = FileMetadata;
