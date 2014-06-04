9ffafe86db122e639001ac137f7a5eb08ead5d6a
Enrico Marchesin <github.com@palio.net>
2014-06-04 12:20:36 +0200

    ios code update

--- src/ios/CDVFileMetadata.m
+++ src/ios/CDVFileMetadata.m
@@ -1,12 +1,33 @@
 #import <Cordova/CDV.h>
 #import "CDVFileMetadata.h"
 
+#import <MagicKit/MagicKit.h>
+
 @implementation CDVFileMetadata
 
 - (id)initWithWebView:(UIWebView*)theWebView
 {
     self = (CDVFileMetadata*)[super initWithWebView:theWebView];
    return self;
+}
+
+- (void)metadata:(CDVInvokedUrlCommand*)command
+{
+    NSArray* arguments = command.arguments;
+
+    // arguments
+    NSString* strFileUrl = [arguments objectAtIndex:0];
+    NSLog(@"metadata(); file url: %@", strFileUrl);
+
+	NSMutableDictionary* r = [NSMutableDictionary dictionaryWithCapacity:2];
+	[r setObject:strFileUrl forKey:@"url"];
+
+	GEMagicResult *magic = [GEMagicKit magicForFileAtPath:strFileUrl];
+	[r setObject:magic.mimeType forKey:@"type"];
+
+	CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:r];
+
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
 - (void)test:(CDVInvokedUrlCommand*)command
