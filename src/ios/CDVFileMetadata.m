c92972b27b2ce3fbdcc94185b42c6cba910a62ff
Enrico Marchesin <github.com@palio.net>
2014-06-05 10:28:00 +0200

    ios implementation completed

--- src/ios/CDVFileMetadata.m
+++ src/ios/CDVFileMetadata.m
@@ -11,22 +11,47 @@
    return self;
 }
 
-- (void)metadata:(CDVInvokedUrlCommand*)command
+- (void)getMetadataForFileURI:(CDVInvokedUrlCommand*)command
 {
     NSArray* arguments = command.arguments;
-
-    // arguments
     NSString* strFileUrl = [arguments objectAtIndex:0];
-    NSLog(@"metadata(); file url: %@", strFileUrl);
 
-	NSMutableDictionary* r = [NSMutableDictionary dictionaryWithCapacity:2];
+	NSMutableDictionary* r = [NSMutableDictionary dictionaryWithCapacity:3];
 	[r setObject:strFileUrl forKey:@"url"];
 
-	GEMagicResult *magic = [GEMagicKit magicForFileAtPath:strFileUrl];
-	[r setObject:magic.mimeType forKey:@"type"];
+	if ([strFileUrl hasPrefix:@"file://"]) {
+		NSURL* urlFile=[NSURL URLWithString:strFileUrl];
+/*
+		NSString* strFilePath = [strFileUrl substringFromIndex:7];
+		NSLog(@"metadata(); file path: %@", strFilePath);
+		[r setObject:strFilePath forKey:@"path"];
+
+		unsigned long long fileSize = [[[NSFileManager defaultManager] attributesOfItemAtPath:strFilePath error:nil] fileSize];
+		NSLog(@"metadata(); file size: %llu", fileSize);
+*/
+		NSError *error = nil;
+		NSNumber* fileSize;
+		if (![urlFile getResourceValue:&fileSize forKey:NSURLFileSizeKey error:&error]) {
+			NSLog(@"name: %@", [error localizedDescription]);
+			[r setObject:[NSNumber numberWithInt:-1] forKey:@"size"];
+			[r setObject:NULL forKey:@"type"];
+		} else {
+			NSLog(@"metadata(); file size: %@", fileSize);
+			[r setObject:fileSize forKey:@"size"];
+
+			GEMagicResult *magic = [GEMagicKit magicForFileAtURL:urlFile];
+			NSString* fullMimeType = magic.mimeType;
+			NSLog(@"metadata(); file type full: %@", fullMimeType);
+			NSString* shortMimeType = [[fullMimeType componentsSeparatedByString:@";"][0] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+			NSLog(@"metadata(); file type short: %@", shortMimeType);
+			[r setObject:shortMimeType forKey:@"type"];
+		}
+	} else {
+		[r setObject:[NSNumber numberWithInt:-1] forKey:@"size"];
+		[r setObject:NULL forKey:@"type"];
+	}
 
 	CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:r];
-
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
