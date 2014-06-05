af8d2b40042cf907b49f9f3394ac5a49eaad6ba9
o.zambotti <o.zambotti@trentorise.eu>
2014-06-05 15:02:32 +0200

    New method under development

--- src/android/FileMetadata.java
+++ src/android/FileMetadata.java
@@ -3,6 +3,8 @@ package it.smartcampuslab.cordova.file;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
+import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
@@ -58,18 +60,48 @@ public class FileMetadata extends CordovaPlugin {
 					callbackContext.success(obj);
 				}
 			}, callbackContext);
+		} else if (action.equals("getMetadataForURL")) {
+			final String url = args.getString(0);
+
+			threadhelper(new FileOp() {
+				public void run() throws JSONException {
+					JSONObject obj = getMetadataForURL(url);
+					callbackContext.success(obj);
+				}
+			}, callbackContext);
 		} else {
-			return false;
-		}
+            return false;
+        }
 
 		return true;
 	}
+
+    private JSONObject getMetadataForURL(String url) throws JSONException {
+      long modified = -1;
+
+      HttpURLConnection.setFollowRedirects(true);
+      HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+      long date = con.getLastModified();
+
+      if (date != 0) {
+        modified = date;
+      }
+
+      JSONObject r = new JSONObject();
+      r.put("url", url);
+      r.put("modified", modified);
+
+      Log.d(LOG_TAG, r.toString());
+
+      return r;
+    }
 
 	private JSONObject getMetadataForFileURI(String filename) throws JSONException, URISyntaxException, IOException {
 		URI uri = new URI(filename);
 		File file = new File(uri);
 		long length = -1;
 		String type = null;
+        long modified = -1;
 
 		if (file.exists()) {
 			length = file.length();
@@ -81,9 +113,10 @@ public class FileMetadata extends CordovaPlugin {
 		}
 
 		JSONObject r = new JSONObject();
-        r.put("url", filename);
+        r.put("uri", filename);
 		r.put("size", length);
 		r.put("type", type);
+        r.put("modified", modified);
 
 		Log.d(LOG_TAG, r.toString());
 
